885079e97783bea9128c4f0186c3609706d7b0f0
anton1gritsenko <tosha.gritzenko@meta.ua>
2018-11-09 13:53:36 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,288 @@
+AmCharts.makeChart("chartdiv1",
+    {
+        "type": "serial",
+        "categoryField": "category",
+        "autoMarginOffset": 40,
+        "marginRight": 60,
+        "marginTop": 60,
+        "startDuration": 1,
+        "fontSize": 13,
+        "theme": "light",
+        "categoryAxis": {
+            "autoRotateAngle": -50.4,
+            "autoRotateCount": 0,
+            "gridPosition": "start",
+            "titleRotation": 0
+        },
+        "trendLines": [],
+        "graphs": [
+            {
+                "balloonText": "[[title]] of [[category]]:[[value]]",
+                "bullet": "bubble",
+                "bulletHitAreaSize": 2,
+                "columnWidth": 0.19,
+                "cornerRadiusTop": 1,
+                "cursorBulletAlpha": 0,
+                "fillAlphas": 1,
+                "fixedColumnWidth": 29,
+                "gapPeriod": 1,
+                "hideBulletsCount": 20,
+                "id": "AmGraph-1",
+                "labelText": "",
+                "legendAlpha": 0,
+                "lineThickness": 10,
+                "negativeLineAlpha": 0,
+                "precision": 2,
+                "title": "Кількість відвідувачів за день",
+                "topRadius": 0,
+                "type": "column",
+                "valueField": "column-1"
+            }
+        ],
+        "guides": [],
+        "valueAxes": [
+            {
+                "id": "ValueAxis-1",
+                "title": "",
+                "titleFontSize": 0
+            }
+        ],
+        "allLabels": [],
+        "balloon": {},
+        "titles": [],
+        "dataProvider": [
+            {
+                "category": "auto.ria.com",
+                "column-1": "0.74"
+            },
+            {
+                "category": "rst.ua",
+                "column-1": "0.46"
+            },
+            {
+                "category": "autobazar.ua",
+                "column-1": "0.03"
+            },
+            {
+                "category": "olx.ua",
+                "column-1": "3.33"
+            },
+            {
+                "category": "autoportal.ua",
+                "column-1": "0.016"
+            },
+            {
+                "category": "uavto.com.ua",
+                "column-1": "0.0008"
+            },
+            {
+                "category": "automoto.ua",
+                "column-1": "0.02"
+            },
+            {
+                "category": "auto.meta.ua",
+                "column-1": "0.06"
+            },
+            {
+                "category": "avtopoisk.ua",
+                "column-1": "0.0008"
+            },
+            {
+                "category": "sale-auto.pp.ua",
+                "column-1": "0.016"
+            }
+        ]
+    }
+);
+AmCharts.makeChart("chartdiv2",
+    {
+        "type": "serial",
+        "categoryField": "category",
+        "autoMarginOffset": 40,
+        "marginRight": 60,
+        "marginTop": 60,
+        "startDuration": 1,
+        "fontSize": 13,
+        "theme": "black",
+        "categoryAxis": {
+            "autoRotateAngle": -50.4,
+            "autoRotateCount": 0,
+            "gridPosition": "start",
+            "titleRotation": 0
+        },
+        "trendLines": [],
+        "graphs": [
+            {
+                "balloonText": "[[title]] of [[category]]:[[value]]",
+                "bullet": "bubble",
+                "bulletHitAreaSize": 2,
+                "columnWidth": 0.19,
+                "cornerRadiusTop": 1,
+                "cursorBulletAlpha": 0,
+                "fillAlphas": 1,
+                "fixedColumnWidth": 29,
+                "gapPeriod": 1,
+                "hideBulletsCount": 20,
+                "id": "AmGraph-1",
+                "labelText": "",
+                "legendAlpha": 0,
+                "lineThickness": 10,
+                "negativeLineAlpha": 0,
+                "precision": 2,
+                "title": "Максимально дороговартісний транспорт",
+                "topRadius": 0,
+                "type": "column",
+                "valueField": "column-1"
+            }
+        ],
+        "guides": [],
+        "valueAxes": [
+            {
+                "id": "ValueAxis-1",
+                "title": "",
+                "titleFontSize": 0
+            }
+        ],
+        "allLabels": [],
+        "balloon": {},
+        "titles": [],
+        "dataProvider": [
+            {
+                "category": "auto.ria.com",
+                "column-1": "3.2"
+            },
+            {
+                "category": "rst.ua",
+                "column-1": "0.1"
+            },
+            {
+                "category": "autobazar.ua",
+                "column-1": "0.5"
+            },
+            {
+                "category": "olx.ua",
+                "column-1": "9"
+            },
+            {
+                "category": "autoportal.ua",
+                "column-1": "0.38"
+            },
+            {
+                "category": "uavto.com.ua",
+                "column-1": "0.4"
+            },
+            {
+                "category": "automoto.ua",
+                "column-1": "0.5"
+            },
+            {
+                "category": "auto.meta.ua",
+                "column-1": "0.45"
+            },
+            {
+                "category": "avtopoisk.ua",
+                "column-1": "0.8"
+            },
+            {
+                "category": "sale-auto.pp.ua",
+                "column-1": "0.05"
+            }
+        ]
+    }
+);
+AmCharts.makeChart("chartdiv3",
+    {
+        "type": "serial",
+        "categoryField": "category",
+        "autoMarginOffset": 40,
+        "marginRight": 60,
+        "marginTop": 60,
+        "startDuration": 1,
+        "fontSize": 13,
+        "theme": "patterns",
+        "categoryAxis": {
+            "autoRotateAngle": -50.4,
+            "autoRotateCount": 0,
+            "gridPosition": "start",
+            "titleRotation": 0
+        },
+        "trendLines": [],
+        "graphs": [
+            {
+                "balloonText": "[[title]] of [[category]]:[[value]]",
+                "bullet": "bubble",
+                "bulletHitAreaSize": 2,
+                "columnWidth": 0.19,
+                "cornerRadiusTop": 1,
+                "cursorBulletAlpha": 0,
+                "fillAlphas": 1,
+                "fixedColumnWidth": 29,
+                "gapPeriod": 1,
+                "hideBulletsCount": 20,
+                "id": "AmGraph-1",
+                "labelText": "",
+                "legendAlpha": 0,
+                "lineThickness": 10,
+                "negativeLineAlpha": 0,
+                "precision": 2,
+                "title": "Мінімально-вартісний транспорт",
+                "topRadius": 0,
+                "type": "column",
+                "valueField": "column-1"
+            }
+        ],
+        "guides": [],
+        "valueAxes": [
+            {
+                "id": "ValueAxis-1",
+                "title": "",
+                "titleFontSize": 0
+            }
+        ],
+        "allLabels": [],
+        "balloon": {},
+        "titles": [],
+        "dataProvider": [
+            {
+                "category": "auto.ria.com",
+                "column-1": "0.1"
+            },
+            {
+                "category": "rst.ua",
+                "column-1": "0.2"
+            },
+            {
+                "category": "autobazar.ua",
+                "column-1": "0.05"
+            },
+            {
+                "category": "olx.ua",
+                "column-1": "1.5"
+            },
+            {
+                "category": "autoportal.ua",
+                "column-1": "0.3"
+            },
+            {
+                "category": "uavto.com.ua",
+                "column-1": "0.1"
+            },
+            {
+                "category": "automoto.ua",
+                "column-1": "0.1"
+            },
+            {
+                "category": "auto.meta.ua",
+                "column-1": "0.1"
+            },
+            {
+                "category": "avtopoisk.ua",
+                "column-1": "0.1"
+            },
+            {
+                "category": "sale-auto.pp.ua",
+                "column-1": "0.1"
+            }
+        ]
+    }
+);
